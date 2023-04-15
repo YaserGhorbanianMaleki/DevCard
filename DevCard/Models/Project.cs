fa63cb5ed5ea5b87e03a4785e4daa2ccfde58204
Yaser gh <GhorbanianMaleki@gmail.com>
2023-04-15 18:22:38 +0430

    Add project files.

---
+++ DevCard/Models/Project.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevCard.Models
+{
+    public class Project
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Client { get; set; }
+        public string Image { get; set; }
+
+        public Project(string name, string description, string client, string image)
+        {
+            Name = name;
+            Description = description;
+            Client = client;
+            Image = image;
+        }
+    }
+}
---
+++ DevCard/ViewComponents/ProjectsViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DevCard.Models;
+
+namespace DevCard.ViewComponents
+{
+    public class ProjectsViewComponent:ViewComponent
+    {
+        private readonly List<DevCard.Models.Project> projects = new List<DevCard.Models.Project>();
+        public IViewComponentResult Invoke(string s)
+        {
+            projects.Add(new Project("Taxi", "تاکسی انلاین", "Yas", "taxi.jpg"));
+            projects.Add(new Project("ZoodFood", "سفارش انلاین غذا", "Yas", "food.jpg"));
+            projects.Add(new Project("HumabResource", "مدیریت منابع انسانی", "Yas", "humanresource.png"));
+            projects.Add(new Project("Turn", "نوبت انلاین", "Yas", "turn.png"));
+            return View("_Projects",projects);
+        }
+    }
+}
